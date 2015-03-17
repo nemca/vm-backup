712f60fd1c4dc9eb6d15e492f5efdb358085f321
Michael Bruskov <nemca@yandex-team.ru>
2015-03-17 12:37:51 +0300

    Add file fuctions logging

---
+++ logging.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+
+#MAILTO=${MAILTO:-root}
+
+_timeNow() {
+	date +"%Y-%m-%d %H:%M:%S"
+}
+
+_log() {
+	if [[ -z $LOG_PATH ]]; then
+		echo `_timeNow` "[INFO] $1"
+	else
+		echo `_timeNow` "[INFO] $1" >> $LOG_PATH
+	fi
+}
+
+_error() {
+	if [[ -z $LOG_PATH ]]; then
+		echo `_timeNow` "[ERROR] $1"
+	else
+		echo `_timeNow` "[ERROR] $1" >> $LOG_PATH
+	fi
+	if [[ -n $MAILTO ]]; then
+		echo `_timeNow` "[ERROR] $1" | mail -s "[ERROR] `basename $0` on `hostname -f`" $MAILTO
+	fi
+	exit 1
+}
+
+_warning() {
+	if [[ -z $LOG_PATH ]]; then
+		echo `_timeNow` "[WARNING] $1"
+	else
+		echo `_timeNow` "[WARNING] $1" >> $LOG_PATH
+	fi
+}
+
+if [[ -n $LOG_PATH ]]; then
+	err_msg=`touch $LOG_PATH 2>&1 >/dev/null`
+	if [[ $? != 0 ]]; then
+		_error "$err_msg"
+	fi
+fi
